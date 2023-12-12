7c0a3a2ce50fb3ae37016a19074aecbaa598ca75
Hamza El Hafyani <h.elhafyani@quinten-france.com>
2023-12-12 20:13:03 +0100

    [ADD] RDS IAM Role authentication

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "${var.AWS_REGION}"
 }
 
 resource "aws_vpc" "myvpc" {
@@ -14,7 +14,7 @@ resource "aws_subnet" "pubsub" {
   cidr_block = "10.0.10.0/24"
   vpc_id = aws_vpc.myvpc.id
   assign_ipv6_address_on_creation = false
-  availability_zone = "eu-west-1a"
+  availability_zone = "${var.AWS_REGION}a"
   depends_on = [aws_vpc.myvpc]
   tags = {
     Name = "Public-Subnet"
@@ -39,7 +39,7 @@ resource "aws_subnet" "privsub" {
   cidr_block = "10.0.20.0/24"
   vpc_id = aws_vpc.myvpc.id
   assign_ipv6_address_on_creation = false
-  availability_zone = "eu-west-1b"
+  availability_zone = "${var.AWS_REGION}b"
   depends_on = [aws_subnet.pubsub]
   tags = {
     Name = "private-subnet"
@@ -52,7 +52,7 @@ resource "aws_eip" "vpc_lan_natgw" {
 
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.vpc_lan_natgw.id
-  subnet_id     = aws_subnet.privsub.id
+  subnet_id     = aws_subnet.pubsub.id
 }
 
 # Example: Route Table for Private Subnet
@@ -75,7 +75,7 @@ resource "aws_subnet" "priv2sub" {
   cidr_block = "10.0.30.0/24"
   vpc_id = aws_vpc.myvpc.id
   assign_ipv6_address_on_creation = false
-  availability_zone = "eu-west-1c"
+  availability_zone = "${var.AWS_REGION}c"
   depends_on = [aws_subnet.privsub]
   tags = {
     Name = "private-subnet2"
@@ -131,7 +131,7 @@ resource "aws_security_group" "sg_backend" {
     from_port = 3000
     protocol = "tcp"
     to_port = 3000
-    cidr_blocks = [aws_subnet.privsub.cidr_block]
+    cidr_blocks = [aws_subnet.pubsub.cidr_block]
   }
 
   ingress {
@@ -139,7 +139,7 @@ resource "aws_security_group" "sg_backend" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_subnet.privsub.cidr_block]
+    cidr_blocks = [aws_subnet.pubsub.cidr_block]
   }
 
   egress {
@@ -155,11 +155,11 @@ resource "aws_security_group" "sg2" {
   vpc_id      = aws_vpc.myvpc.id
 
   ingress {
-    description = "MySQL"
-    from_port = 3306
+    description = "PSQL"
+    from_port = 5432
     protocol = "tcp"
-    to_port = 3306
-    cidr_blocks = [aws_vpc.myvpc.cidr_block]
+    to_port = 5432
+    cidr_blocks = [aws_subnet.privsub.cidr_block, aws_subnet.priv2sub.cidr_block]
   }
 
   egress {
@@ -185,21 +185,21 @@ resource "aws_key_pair" "ec2key_pair" {
 }
 
 resource "aws_secretsmanager_secret" "ec2key_secrets" {
-   name = "key_ec2"
-   description = "ec2key_vm_private_key"
+   name = "${var.SSH_KEY_SECRET_NAME}"
+   description = "${var.SSH_PRIVATE_KEY_JSON_KEY}"
 }
 
 resource "aws_secretsmanager_secret_version" "ec2key_secrets" {
   secret_id = aws_secretsmanager_secret.ec2key_secrets.id
   secret_string = jsonencode(
    {
-    "ec2key_vm_private_key": "${tls_private_key.ec2key_tls_private_key.private_key_pem}"
+    "${var.SSH_PRIVATE_KEY_JSON_KEY}": "${tls_private_key.ec2key_tls_private_key.private_key_pem}"
    }
   )
 }
 
 resource "aws_secretsmanager_secret" "rds_password" {
-  name = "password_rds"
+  name = "${var.RDS_SECRET_NAME}"
 
   tags = {
     Name        = "RDS_Password"
@@ -229,7 +229,7 @@ resource "aws_instance" "frontend" {
   ami = "ami-0eb11ab33f229b26c"
   instance_type = "t2.micro"
   associate_public_ip_address = true
-  availability_zone = "eu-west-1a"
+  availability_zone = "${var.AWS_REGION}a"
   depends_on = [aws_security_group.sg2, aws_instance.backend]
   key_name = aws_key_pair.ec2key_pair.key_name
   subnet_id = "${aws_subnet.pubsub.id}"
@@ -258,11 +258,12 @@ resource "aws_instance" "frontend" {
 resource "aws_instance" "backend" {
   ami = "ami-0eb11ab33f229b26c"
   instance_type = "t2.micro"
-  availability_zone = "eu-west-1b"
+  availability_zone = "${var.AWS_REGION}b"
   #depends_on = [aws_security_group.sg2]
   key_name = aws_key_pair.ec2key_pair.key_name
   subnet_id = "${aws_subnet.privsub.id}"
   vpc_security_group_ids = [aws_security_group.sg_backend.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
   tags = {
     Name = "backend"
   }
@@ -270,6 +271,7 @@ resource "aws_instance" "backend" {
               #!/bin/bash
               sudo apt-get update -y
               sudo apt-get install -y docker.io
+              sudo apt-get install -y postgresql-client
               
               # Clone from GitHub
               sudo rm -rf /opt/hi.health
@@ -280,7 +282,7 @@ resource "aws_instance" "backend" {
               sudo docker build -t backend -f docker/backend.Dockerfile .
 
               # Run Docker container
-              sudo docker run --name backend -e DB_HOST="${aws_db_instance.database.endpoint}" -e DB_DATABASE="app" -e DB_USERNAME="hihealth" -e DB_PASSWORD="${aws_secretsmanager_secret_version.rds_password_version.secret_string}" -d -p 3000:3000 backend:latest
+              sudo docker run --name backend -e DB_HOST="${aws_db_instance.database.endpoint}" -e DB_DATABASE="${aws_db_instance.database.db_name}" -e DB_USERNAME="${aws_db_instance.database.username}" -d -p 3000:3000 backend:latest
               EOF
 }
 
@@ -288,7 +290,6 @@ resource "aws_instance" "backend" {
 resource "aws_db_subnet_group" "private" {
   name       = "privatedbsubgroup"
   subnet_ids = [aws_subnet.privsub.id, aws_subnet.priv2sub.id]
-  #depends_on = [aws_instance.frontend]
 
   tags = {
     Name = "My Private DB subnet group"
@@ -297,18 +298,68 @@ resource "aws_db_subnet_group" "private" {
 
 resource "aws_db_instance" "database" {
   depends_on = [aws_db_subnet_group.private]
-  db_name = "mydb"
+  db_name = "${var.DB_NAME}"
   instance_class = "db.t2.micro"
   allocated_storage = 10
-  availability_zone = "eu-west-1b"
+  availability_zone = "${var.AWS_REGION}b"
   engine = "postgres"
   engine_version = "12.17"
-  username = "hihealth"
+  username = "${var.DB_USER}"
   password = aws_secretsmanager_secret_version.rds_password_version.secret_string
   skip_final_snapshot = true
   db_subnet_group_name = aws_db_subnet_group.private.name
   port = 5432
   iam_database_authentication_enabled = true
+}
+
+# Create IAM user
+resource "aws_iam_user" "db_user" {
+  name = "${var.DB_USER}"
+}
+
+# Create IAM policy that maps the database user to an IAM role
+resource "aws_iam_policy" "rds_mapping_policy" {
+  name        = "rds_mapping_policy"
+  description = "Policy for mapping IAM user to database user"
+  
+  # Define policy statements here
+  # Example: Allow connecting to the RDS instance
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action   = "rds-db:connect",
+      Effect   = "Allow",
+      Resource = "${aws_db_instance.database.arn}/${aws_iam_user.db_user.name}",
+    }],
+  })
+}
+
+# Attach the IAM policy to the IAM user
+resource "aws_iam_user_policy_attachment" "attach_mapping_policy" {
+  user       = aws_iam_user.db_user.name
+  policy_arn = aws_iam_policy.rds_mapping_policy.arn
+}
+
+# Create IAM role for EC2 instance
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com",
+      },
+    }],
+  })
+}
+
+# Create IAM instance profile
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_role.name
 }
 
 resource "aws_default_security_group" "default" {
