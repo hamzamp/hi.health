3e2d7dbbc945060ae385a1072807b0034448a41e
Hamza El Hafyani <h.elhafyani@quinten-france.com>
2023-12-12 20:13:36 +0100

    [ADD] vars file for terraform

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_REGION" {
+}
+variable "DB_NAME" {
+}
+variable "DB_USER" {
+}
+variable "RDS_SECRET_NAME" {
+}
+variable "SSH_KEY_SECRET_NAME" {
+}
+variable "SSH_PRIVATE_KEY_JSON_KEY" {
+}
